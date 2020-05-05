a224ac9f735e2d9c29fc68f92c260746d92e2a6e
walz0 <aidan.m.walz@gmail.com>
2020-05-04 21:14:20 -0400

    Filtered participating Indiana teams

--- main.py
+++ main.py
@@ -39,9 +39,10 @@ class frc_event():
         return self.__str__()
 
 class frc_team():
-    def __init__(self, key, name):
+    def __init__(self, key, name, state):
         self.key = key 
         self.name = name 
+        self.state = state
 
     def __str__(self):
         return self.key + ": ({})".format(self.name)
@@ -56,11 +57,14 @@ def callAPI(query):
     response = requests.get(url, headers=headers)
     return response.json()
 
+total_teams = 3898 # total number of active frc teams (2020)
+
 sus_events = [] # suspended events
 com_events = [] # completed events
 all_events = [] # all events
 
 active_teams = [] # all teams that have competed in at least one event
+in_teams = [] # all indiana teams that have competed
 
 # pull all events planned for 2020
 events = callAPI('events/2020')
@@ -73,8 +77,12 @@ for event in events:
     else:
         com_events.append(frc_event(event['key'], event['name'], event['week']))
         teams = callAPI('event/' + event['key'] + '/teams')
-        active_teams.append()
-
+        for team in teams:
+            obj = frc_team(team['key'], team['name'], team['state_prov'])
+            if not obj in active_teams:
+                active_teams.append(obj)
+                if obj.state == "Indiana":
+                    in_teams.append(obj)
     all_events += [(event['key'], event['name'])]
 
 print("Total Events Scheduled: ", len(all_events))
@@ -82,4 +90,7 @@ print("Total Events Suspended: ", len(sus_events))
 print("Percent Suspended: ", 100 * (len(sus_events) / len(all_events)))
 
 #pprint.pprint(com_events)
-pprint.pprint(active_teams)
+#pprint.pprint(active_teams)
+print("Total Teams Participated: ", len(active_teams))
+print("Total Percent Participated: ", 100 * (len(active_teams) / total_teams))
+print("Total Indiana Teams Participated: ", len(in_teams))
